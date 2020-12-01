062e4c1a946e52870bb36e1fe9c83b5df571c072
Clint Olson <coandco@gmail.com>
2020-12-01 00:23:58 -0800

    Refactored day 1 to be prettier

--- advent2020_day01.py
+++ advent2020_day01.py
@@ -1,12 +1,15 @@
 import itertools
 from utils import read_data
+from math import prod
 
-input_processed = [int(x) for x in read_data().split("\n")]
 
-combinations = itertools.combinations(input_processed, 2)
-sums = [(x[0] + x[1], x[0] * x[1]) for x in combinations if x[0] + x[1] == 2020]
-print(sums[0][1])
+def sum_prod(num_list, sum_value, r_length):
+    combinations = itertools.combinations(num_list, r_length)
+    solution = [prod(x) for x in combinations if sum(x) == sum_value]
+    return solution[0] if solution else None
 
-threefold = list(itertools.combinations(input_processed, 3))
-sums = [(x[0] + x[1] + x[2], x[0] * x[1] * x[2]) for x in threefold if x[0] + x[1] + x[2] == 2020]
-print(sums[0][1])
+
+if __name__ == '__main__':
+    input_processed = [int(x) for x in read_data().split("\n")]
+    print(sum_prod(input_processed, 2020, 2))
+    print(sum_prod(input_processed, 2020, 3))
