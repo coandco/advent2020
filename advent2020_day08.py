ae9481b5f01f7f8721181901ec4e503ce8e4f552
Clint Olson <coandco@gmail.com>
2020-12-08 16:34:04 -0800

    Use NamedTuple and program copies

--- advent2020_day08.py
+++ advent2020_day08.py
@@ -1,20 +1,29 @@
 from utils import read_data
-from typing import List
+from typing import List, NamedTuple
 
 
-class Instruction:
-    def __init__(self, opcode, arg):
-        self.opcode = opcode
-        self.arg = int(arg)
+class Instruction(NamedTuple):
+    opcode: str
+    arg: int
+
+    @staticmethod
+    def from_string(string):
+        opcode, arg = string.split(" ")
+        return Instruction(opcode, int(arg))
+
+    def swap_opcode(self):
+        if self.opcode == "jmp":
+            return Instruction("nop", self.arg)
+        elif self.opcode == "nop":
+            return Instruction("jmp", self.arg)
+        else:
+            return self
 
     def __repr__(self):
         return f"{self.opcode} {self.arg}"
 
 
-INPUT = read_data().split("\n")
-
-
-def run_program(data: List[Instruction], swap_number: int):
+def run_program(data: List[Instruction]):
     pc = 0
     acc = 0
     seen_instructions = set()
@@ -25,24 +34,28 @@ def run_program(data: List[Instruction], swap_number: int):
         if current.opcode == "acc":
             acc += current.arg
             pc += 1
-        elif (current.opcode == "jmp" and pc != swap_number) or (current.opcode == "nop" and pc == swap_number):
+        elif current.opcode == "jmp":
             pc += current.arg
-        elif (current.opcode == "nop" and pc != swap_number) or (current.opcode == "jmp" and pc == swap_number):
+        elif current.opcode == "nop":
             pc += 1
         if pc in seen_instructions:
             return True, acc
         if pc >= len(data):
             return False, acc
 
-data = [Instruction(*x.split(" ")) for x in INPUT]
 
-_, acc = run_program(data, -1)
+INPUT = read_data().split("\n")
+data = [Instruction.from_string(x) for x in INPUT]
+
+_, acc = run_program(data)
 
 print(f"Part one accumulator is {acc}")
 
 current_swap = 0
 while True:
-    looped, acc = run_program(data, current_swap)
+    modified_program = data[:]
+    modified_program[current_swap] = modified_program[current_swap].swap_opcode()
+    looped, acc = run_program(modified_program)
     if looped:
         # print(f"swapping instruction {current_swap} results in a loop")
         pass
