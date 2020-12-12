97e7a364b9f6c6022f8a3a2fdfe989e45916d1a5
Clint Olson <coandco@gmail.com>
2020-12-11 23:08:19 -0800

    Minor cleanup for day 12

--- advent2020_day12.py
+++ advent2020_day12.py
@@ -1,19 +1,24 @@
 from utils import read_data
-from typing import NamedTuple
+from typing import NamedTuple, List
 
 
 class Coord(NamedTuple):
     y: int
     x: int
 
+    @property
+    def manhattan_distance(self) -> int:
+        return abs(self.x) + abs(self.y)
+
     def __add__(self, other: 'Coord') -> 'Coord':
         return Coord(y=self.y + other.y, x=self.x + other.x)
 
-    def __mul__(self, amount: int):
+    def __mul__(self, amount: int) -> 'Coord':
         return Coord(y=self.y*amount, x=self.x*amount)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Coord(y={self.y}, x={self.x})"
+
 
 DIRECTIONS = {
     'N': Coord(y=-1, x=0),
@@ -37,77 +42,79 @@ LEFT_TURN = {
 }
 
 
-def move_direction(curpos: Coord, direction: str, amount: int):
+def move_direction(curpos: Coord, direction: str, amount: int) -> Coord:
     return curpos + (DIRECTIONS[direction] * amount)
 
 
-def turn_right(current_heading: str, times: int):
+def turn_right(current_heading: str, times: int) -> str:
     new_heading = current_heading
     for _ in range(times):
         new_heading = RIGHT_TURN[new_heading]
     return new_heading
 
 
-def turn_left(current_heading: str, times: int):
+def turn_left(current_heading: str, times: int) -> str:
     new_heading = current_heading
     for _ in range(times):
         new_heading = LEFT_TURN[new_heading]
     return new_heading
 
 
-def rotate_right(point: Coord, times: int):
+def rotate_right(point: Coord, times: int) -> Coord:
     new_point = point
     for _ in range(times):
         new_point = Coord(x=-new_point.y, y=new_point.x)
     return new_point
 
 
-def rotate_left(point: Coord, times: int):
+def rotate_left(point: Coord, times: int) -> Coord:
     new_point = point
     for _ in range(times):
         new_point = Coord(x=new_point.y, y=-new_point.x)
     return new_point
 
 
-def part_one(data):
+def part_one(data: List[str]) -> Coord:
     current_loc = Coord(0, 0)
     current_heading = 'E'
     for line in data:
-        if line[0] in DIRECTIONS.keys():
-            current_loc = move_direction(current_loc, line[0], int(line[1:]))
-        elif line[0] == 'R':
-            num_turns = int(line[1:]) // 90
+        op, value = line[0], int(line[1:])
+        if op in DIRECTIONS.keys():
+            current_loc = move_direction(current_loc, op, value)
+        elif op == 'R':
+            num_turns = (value // 90) % 4
             current_heading = turn_right(current_heading, num_turns)
-        elif line[0] == 'L':
-            num_turns = int(line[1:]) // 90
+        elif op == 'L':
+            num_turns = (value // 90) % 4
             current_heading = turn_left(current_heading, num_turns)
-        elif line[0] == 'F':
-            current_loc = move_direction(current_loc, current_heading, int(line[1:]))
+        elif op == 'F':
+            current_loc = move_direction(current_loc, current_heading, value)
     return current_loc
 
 
-def part_two(data):
+def part_two(data: List[str]) -> Coord:
     ship_loc = Coord(0, 0)
     waypoint_loc = Coord(y=-1, x=10)
     for line in data:
-        if line[0] in DIRECTIONS.keys():
-            waypoint_loc = move_direction(waypoint_loc, line[0], int(line[1:]))
-        elif line[0] == 'R':
-            num_rotations = int(line[1:]) // 90
+        op, value = line[0], int(line[1:])
+        if op in DIRECTIONS.keys():
+            waypoint_loc = move_direction(waypoint_loc, op, value)
+        elif op == 'R':
+            num_rotations = (value // 90) % 4
             waypoint_loc = rotate_right(waypoint_loc, num_rotations)
-        elif line[0] == 'L':
-            num_rotations = int(line[1:]) // 90
+        elif op == 'L':
+            num_rotations = (value // 90) % 4
             waypoint_loc = rotate_left(waypoint_loc, num_rotations)
-        elif line[0] == 'F':
-            ship_loc += (waypoint_loc * int(line[1:]))
+        elif op == 'F':
+            ship_loc += (waypoint_loc * value)
     return ship_loc
 
 
 INPUT = read_data().split("\n")
 
 part_one_loc = part_one(INPUT)
-print(f"Part one: the ship is at {part_one_loc}, a Manhattan distance of {abs(part_one_loc.x) + abs(part_one_loc.y)}")
+print(f"Part one: the ship is at {part_one_loc}, a Manhattan distance of {part_one_loc.manhattan_distance}")
 
 part_two_loc = part_two(INPUT)
-print(f"Part two: the ship is at {part_two_loc}, a Manhattan distance of {abs(part_two_loc.x) + abs(part_two_loc.y)}")
+print(f"Part two: the ship is at {part_two_loc}, a Manhattan distance of {part_two_loc.manhattan_distance}")
 
