7b9da42b6ea33f18126ad0856837c35da1bf8a53
Clint Olson <coandco@gmail.com>
2020-12-13 22:12:20 -0800

    Comment and clean up day 14

--- advent2020_day14.py
+++ advent2020_day14.py
@@ -12,18 +12,26 @@ def part_one(data: List[str]) -> int:
         else:
             address = int(op[4:-1])
             binary_value = f"{int(value):036b}"
+            # Apply the mask by selecting the value for each position from one of the two strings,
+            # depending on if it's an X or not
             masked_value = "".join(binary_value[x] if mask[x] == "X" else mask[x] for x in range(len(binary_value)))
             mem[address] = int(masked_value, 2)
     return sum(mem.values())
 
 
 def generate_possible_values(floating_binary: str):
+    # Find the index of each X in the input string
     x_indices = [i for i, x in enumerate(floating_binary) if x == "X"]
+    # If we have 6 values, loop from 0 to 2^6 so we hit all combinations of 0 and 1 for them
     for i in range(0, pow(2, len(x_indices))):
+        # Turn the i into a binary string with the same length as x_indices
         binary_replacements = f"{i:0b}".zfill(len(x_indices))
+        # Convert the original string into a list so we can modify it
         result = list(floating_binary)
         for i, x in enumerate(x_indices):
+            # Replace a single X in the floating binary with its replacement
             result[x] = binary_replacements[i]
+        # Convert the result back to an int before we yield it
         yield int("".join(result), 2)
 
 
@@ -35,9 +43,12 @@ def part_two(data: List[str]) -> int:
         if op == "mask":
             mask = value
         else:
-            binary_address = f"{int(op[4:-1]):036b}"
-            masked_address = "".join(
-                binary_address[x] if mask[x] == "0" else mask[x] for x in range(len(binary_address)))
+            premask_address = int(op[4:-1])
+            binary_address = f"{premask_address:036b}"
+            # Only if the mask is a zero do we use the original.  Otherwise, we overwrite it with the mask.
+            masked_address = "".join(binary_address[x] if mask[x] == "0" else mask[x]
+                                     for x in range(len(binary_address)))
+
             for address in generate_possible_values(masked_address):
                 mem[address] = int(value)
     return sum(mem.values())
