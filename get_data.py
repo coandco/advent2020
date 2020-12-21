07112514e6c9b6ef476f4c64be45a75d9a63107b
Clint Olson <coandco@gmail.com>
2020-12-20 20:46:02 -0800

    Update get_data.py to autocreate the program template

--- get_data.py
+++ get_data.py
@@ -5,9 +5,13 @@ from pathlib import Path
 # for a given year/day and dump it into an automatically-named file.
 
 YEAR_NUM = 2020
-DAY_NUM = 1
+DAY_NUM = 20
 
 puzzle = Puzzle(year=YEAR_NUM, day=DAY_NUM)
 
 file_location = Path(f'inputs/advent{YEAR_NUM}_day{DAY_NUM:02d}_input.txt')
 file_location.write_text(puzzle.input_data)
+
+program_location = Path(f"advent{YEAR_NUM}_day{DAY_NUM:02d}.py")
+if not program_location.exists():
+    program_location.write_text("from utils import read_data")
